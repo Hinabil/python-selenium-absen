787b9c89ea7c840021a0c126e6021839622aa5d1
Hinabil <nabilmahbub@gmail.com>
2025-08-19 02:32:59 +0700

    haapus path

--- src/absen.py
+++ src/absen.py
@@ -16,10 +16,16 @@ URL_LOGOUT = "https://simkuliah.usk.ac.id/index.php/login/logout"
 users = [
     ("Nabil Mahbub", "2404111010055", "15927386"),
     ("Muhammad Afzalul Affan", "2404108010067", "48159367"),
+    ("Luna Selviana","2404106010001","luna181920"),
+    ("Naufal Aziz","2406102010046","24139758"),
+    ("Febrilia Ananda Putri","2404106010070","febri070"),
+    ("Adrian Rezky Duana ","Rezky","Alish@275"),
+    ("balqis athira djumayardi ","2406104030044","58674231"),
+    ("Muhammad Aidil Fashsha","2404111010045","56378941"),
 ]
 
 # --- SETUP CHROME DRIVER ---
-chrome_service = Service()  # Tidak perlu path, gunakan default
+# chrome_service = Service()  # Hapus baris ini
 chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
@@ -61,7 +67,7 @@ def absen(driver, nama):
         time.sleep(1)
 
 def main():
-    driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)  # Hapus argumen service
     driver.set_window_size(1280, 800)
     try:
         for nama, username, password in users:
